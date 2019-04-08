5df33ba707bcfed9608c6ee62100ce502adfe4ee
Brett Patterson <brett.patterson@effem.com>
2019-04-07 20:28:23 -0400

    Fix typo in model name
    Signed-off-by: Brett Patterson <brett.patterson@effem.com>

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :redmine_configurable_scm_ticket_id_prefix do
 	name 'Redmine Configurable SCM Ticket ID Prefix'
 	author 'Brett Patterson'
 	description 'Allow admin to change the ticket ID prefix from "#" to something else to help resolve conflicts of other software reading the commit messages to reference tickets'
-	version '1.0.0'
+	version '1.0.1'
 
 	settings :default => {'empty' => true}, :partial => 'settings/ticket_id_prefix_settings'
 
@@ -17,6 +17,6 @@ Rails.configuration.to_prepare do
 
   require_dependency 'changeset'
   unless Changeset.included_modules.include?(RedmineConfigurableScmTicketIdPrefix::Patches::ChangesetPatch)
-    Change.send(:prepend, RedmineConfigurableScmTicketIdPrefix::Patches::ChangesetPatch)
+    Changeset.send(:prepend, RedmineConfigurableScmTicketIdPrefix::Patches::ChangesetPatch)
   end
 end
